4e566093f85bfc1430614eb58f700ec443756062
Mazur Roman <superioritywork@gmail.com>
2023-05-20 16:28:51 +0200

    transfer 02hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Section from './Section';
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Notification from './Notification';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  total = 0;
+  incrementReviews = event => {
+    const { textContent } = event.currentTarget;
+    let name = textContent.toLowerCase();
+
+    this.countTotalFeedback();
+
+    this.setState(prevState => {
+      return {
+        [name]: prevState[name] + 1,
+      };
+    });
+  };
+  countTotalFeedback = () => {
+    this.total += 1;
+  };
+  countPositiveFeedbackPercentage = () =>
+    Math.round((this.state.good / this.total) * 100);
+
+  render() {
+    return (
+      <div className={css.feedback}>
+        <div>
+          <Section title="Please leave feedback"></Section>
+          <FeedbackOptions
+            options={['Good', 'Neutral', 'Bad']}
+            onLeaveFeedback={this.incrementReviews}
+          ></FeedbackOptions>
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.total}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          ></Statistics>
+          {this.total === 0 && (
+            <Notification message="There is no feedback"></Notification>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
